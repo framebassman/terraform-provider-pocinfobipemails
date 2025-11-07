9403e7acfeb31d599ff95484e28570f005b799f7
Dmitry Romashov <framebassman@gmail.com>
2025-11-07 14:04:22 +0100

    Logging

---
+++ examples/logging/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    pocinfobipemails = {
+      source = "hashicorp.com/edu/pocinfobipemails"
+    }
+  }
+}
+
+variable "infobip_base_url" {}
+variable "infobip_api_key" {}
+
+provider "pocinfobipemails" {
+  base_url = var.infobip_base_url
+  api_key  = var.infobip_api_key
+}
+
+data "pocinfobipemails_email_templates" "edu" {}
